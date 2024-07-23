ca0f5224be62b0c60b03e16ca21f64246a887bcd
Timur <110349339+T1murCoder@users.noreply.github.com>
2024-07-23 14:06:11 +0300

    Added copy login on long press in profile

--- app/src/main/java/ru/technosopher/attendancelogappstudents/ui/utils/Utils.java
+++ app/src/main/java/ru/technosopher/attendancelogappstudents/ui/utils/Utils.java
@@ -1,6 +1,8 @@
 package ru.technosopher.attendancelogappstudents.ui.utils;
 
+import android.content.Context;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -9,5 +11,12 @@ public class Utils {
 
     public static int visibleOrGone(boolean isVisible){
         return isVisible ? View.VISIBLE : View.GONE;
+    }
+
+    public static void setClipboard(String text, Context context) {
+        android.content.ClipboardManager clipboard = (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Text", text);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(context, "Скопировано!", Toast.LENGTH_SHORT).show();
     }
 }
